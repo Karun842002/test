82e1ba8fbedbbbb7d9d01b4d7c525a8a0987cb98
Karun Anantharaman <karun19049@cse.ssn.edu.in>
2021-05-27 22:43:29 +0530

    final

---
+++ src/app/src/Navbar.js
@@ -0,0 +1,14 @@
+import './navbar.css'
+
+export function Navbar() {
+  return (
+    <div class="navbar-container">
+      <style>
+        @import url('https://fonts.googleapis.com/css2?family=Allerta+Stencil&display=swap');
+</style>
+      <span class="logo">TODO</span>
+    </div>
+  )
+}
+
+export default Navbar;
